1a00577ca9f44760c7ece306c5218532ee197f15
dev.cappp <dev.cappp@gmail.com>
2023-09-18 12:40:20 +0900

    feat: json파일 로드 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import Header from '@/components/common/Header';
 import MapScene from '@/components/common/home/MapScene';
-
-export default function Home() {
+import { Info } from '@/types/info';
+import { useEffect } from 'react';
+interface IProps {
+  infos: Info[];
+}
+export default function Home({ infos }: IProps) {
+  useEffect(() => {
+    console.log(infos);
+  }, [infos]);
   return (
     <>
       <Header />
       <MapScene />
     </>
   );
+}
+// pre-rendering 하여서 SSG 생성
+export async function getStaticProps() {
+  // public 에 있는 info.json 가져오기
+  const infos = (await import('@/public/info.json')).default;
+  return {
+    props: { infos },
+    revalidate: 3600,
+  };
 }
--- types/info.ts
+++ types/info.ts
@@ -2,3 +2,8 @@
 type Lat = number;
 type Lng = number;
 export type Cordinates = [Lat, Lng];
+
+// json에서 불러올 데이터 타입 정의
+export type Info = {
+  cordinates: Cordinates;
+};
